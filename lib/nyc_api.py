2a0cfbe32f3b943f3201f74e3cfc951c9e5a6139
De65 <kyalodiana6@gmail.com>
2023-06-22 06:58:48 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,14 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
+  def get_programs():
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
     return response.content
 
 
-programs = GetPrograms().get_programs()
+programs = GetPrograms.get_programs()
 print(programs)
